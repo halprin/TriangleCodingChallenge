4fc2f2b213b3ddc70dc353cd49cea45abf6d5e47
halprin <halprin@users.noreply.github.com>
2017-03-07 12:24:55 -0700

    Added the triangle test

---
+++ src/test/java/net/atPAK/triangle/model/TriangleTest.java
@@ -0,0 +1,33 @@
+package net.atPAK.triangle.model;
+
+import org.junit.Test;
+
+/**
+ * Created by halprin on 3/7/17.
+ */
+public class TriangleTest
+{
+	@Test
+	public void testCreatingEquilateralTriangle()
+	{
+		
+	}
+
+	@Test
+	public void testCreatingIsoscelesTriangle()
+	{
+
+	}
+
+	@Test
+	public void testCreatingScaleneTriangle()
+	{
+
+	}
+
+	@Test
+	public void testCreatingInvalidTriangle()
+	{
+
+	}
+}
