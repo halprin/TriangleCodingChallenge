348ed08dd8490996cc728c944c1a5866d1f4b47f
halprin <halprin@users.noreply.github.com>
2017-03-07 11:27:08 -0700

    Added basic Maven file with a basic main class.

---
+++ src/main/java/net/atPAK/triangle/TriangleMain.java
@@ -0,0 +1,12 @@
+package net.atPAK.triangle;
+
+/**
+ * Created by halprin on 3/7/17.
+ */
+public class TriangleMain
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello Triangle");
+	}
+}
