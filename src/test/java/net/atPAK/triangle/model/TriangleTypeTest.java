d61850a0c3cc581e23ba3907f6ad5b0ceee7c5f8
halprin <halprin@users.noreply.github.com>
2017-03-07 11:51:12 -0700

    Added tests and basic TriangleType enum

--- src/test/java/net/atPAK/triangle/model/TriangleTypeTest.java
+++ src/test/java/net/atPAK/triangle/model/TriangleTypeTest.java
@@ -1,9 +1,23 @@
 package net.atPAK.triangle.model;
 
+import org.junit.Test;
+
 /**
  * Created by halprin on 3/7/17.
  */
 public class TriangleTypeTest
 {
-	
+	private TriangleType objectUnderTest;
+
+	@Test
+	public void testEnumCount()
+	{
+
+	}
+
+	@Test
+	public void testSpecificEnumValues()
+	{
+
+	}
 }
